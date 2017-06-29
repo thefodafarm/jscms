7b75d2e1a8000c27faa7af14e581c2f215d92280
Dillon Raphael <dillonraphael@gmail.com>
2017-06-29 15:58:04 -0400

    Server responds with json && added axios

--- core/Server.js
+++ core/Server.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import session from 'express-session'
+import bodyParser from 'body-parser'
+
 import routes from './routes/index.js'
 
 const app = express();
 
-// app.use('/', routes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes);
 
 app.set('port', process.env.PORT || 1337);
 const server = app.listen(app.get('port'), () => {
